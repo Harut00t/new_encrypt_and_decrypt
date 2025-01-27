dc1575d8492d210bef6622ad2129fdbd8530533e
@Harut00t <seiichipaiko@iskolarngbayan.pup.edu.ph>
2025-01-27 16:16:29 +0800

    This is the pseudocode/step-by-step of my program, will modify it later on.

---
+++ encrypt_for_passwords.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+#variable characters to categorize the characters
+
+#encryption
+
+#print the encrypted word and the randomized characters
+
+#write the date encrypted word, and the randomized characters in a .txt file document for safekeep 
